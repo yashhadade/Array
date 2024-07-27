e642cc40747c66921b5351f5dd7130ce10573786
Yash <77961742+yashhadade@users.noreply.github.com>
2024-07-27 11:47:37 +0530

    Update subArraSum.java

--- subArraSum.java
+++ subArraSum.java
@@ -35,3 +35,19 @@ public static void main (String [] args) {
 	SubArrSum(arr,sum);
 }
 
+	//2nd approch
+
+	/*
+	class Solution {
+    public int[]  twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target - nums[i])){
+             return new int[] {map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i);
+        }
+    return new int[] {};
+    }
+}
+*/
